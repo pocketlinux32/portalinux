269ac3d0533eec6a46c9fb92a1ae1ef7d1c8d641
pocketlinux32 <pocketlinux32@gmail.com>
2021-03-04 14:57:43 -0600

    Installer

--- src/internal-inst/main.c
+++ src/internal-inst/main.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <unistd.h>
-#include <sys/stas.h>
-#include <sys/types.h>
+#include "ncurses-ui.h"
+
+int main(){
+	
+}
--- src/internal-inst/ncurses-ui.h
+++ src/internal-inst/ncurses-ui.h
@@ -3,12 +3,14 @@
 #include <limits.h>
 
 struct currentMenu {
-	MENU* menu
-	ITEM** items
-	void deallocMenu(){
-		
-	};
-	void createMenu(){
-		
-	};
+	MENU* menu;
+	ITEM** items;
+	int itemAmount = 0;
+}
+
+void deallocMenu(){
+	free_menu(currentMenu.menu);
+	for(int i = 0; i < currentMenu.itemAmount; i++){
+		free_item(currentMenu.items[i]);
+	}
 }
